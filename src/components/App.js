25a76f63fa9c37f1b1a2aa18892b6316233bb7f8
Harsh18894 <harshdeep18894@gmail.com>
2020-04-19 23:03:25 +0530

    Front End - Part One

--- src/components/App.js
+++ src/components/App.js
@@ -1,48 +1,48 @@
 import React, { Component } from 'react';
-import logo from '../logo.png';
+import Web3 from 'web3';
 import './App.css';
+import Navbar from './Navbar';
 
 class App extends Component {
+
+  async componentWillMount() {
+    await this.loadWeb3()
+    await this.loadBlockchainData()
+  }
+
+  async loadWeb3() {
+    // Modern dapp browsers...
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum);
+    }
+    // Legacy dapp browsers...
+    else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider);
+    }
+    // Non-dapp browsers...
+    else {
+      window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!');
+    }
+  }
+
+  async loadBlockchainData() {
+    const web3 = window.web3
+    // Load account
+    const accounts = await web3.eth.getAccounts()
+    this.setState({ account: accounts[0] })
+  }
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      account: ''
+    }
+  }
+
   render() {
     return (
       <div>
-        <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-          <a
-            className="navbar-brand col-sm-3 col-md-2 mr-0"
-            href="https://github.com/Harsh18894/blockchain-social-network"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Social Network Project
-          </a>
-        </nav>
-        <div className="container-fluid mt-5">
-          <div className="row">
-            <main role="main" className="col-lg-12 d-flex text-center">
-              <div className="content mr-auto ml-auto">
-                <a
-                  href="https://github.com/Harsh18894/blockchain-social-network"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <img src={logo} className="App-logo" alt="logo" />
-                </a>
-                <h1>Social Network Starter Kit</h1>
-                <p>
-                  Edit <code>src/components/App.js</code> and save to reload.
-                </p>
-                <a
-                  className="App-link"
-                  href="https://github.com/Harsh18894/blockchain-social-network"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <u><b>View Repo </b></u>
-                </a>
-              </div>
-            </main>
-          </div>
-        </div>
+        <Navbar account={this.state.account} />
       </div>
     );
   }
