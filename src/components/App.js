ca9571079de0ddccd80727df65a25b907a8361bc
Harsh18894 <harshdeep18894@gmail.com>
2020-04-20 03:19:35 +0530

    Completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import Web3 from 'web3';
+import SocialNetwork from '../abis/SocialNetwork.json';
 import './App.css';
 import Navbar from './Navbar';
+import Main from './Main';
 
 class App extends Component {
 
@@ -30,19 +32,77 @@ class App extends Component {
     // Load account
     const accounts = await web3.eth.getAccounts()
     this.setState({ account: accounts[0] })
+
+    // Network Id
+    const networkId = await web3.eth.net.getId()
+    const networkData = SocialNetwork.networks[networkId]
+    if (networkData) {
+      //Adress
+      const socialNetwork = web3.eth.Contract(SocialNetwork.abi, networkData.address)
+      this.setState({ socialNetwork })
+      const postCount = await socialNetwork.methods.postCount().call()
+      this.setState({ postCount })
+      // Loop Posts
+      for (var i = 1; i <= postCount; i++) {
+        const post = await socialNetwork.methods.posts(i).call()
+        this.setState({
+          posts: [...this.state.posts, post]
+        })
+      }
+
+      // Sort posts as per tips
+      this.setState({
+        posts: this.state.posts.sort((a, b) => b.tipAmount - a.tipAmount)
+      })
+      this.setState({ loading: false })
+    } else {
+      window.alert('Social Network contract is not deployed on the blockchain')
+    }
+
+    // ABI
+  }
+
+  createPost(content) {
+    this.setState({ loading: true })
+    this.state.socialNetwork.methods.createPost(content).send({ from: this.state.account })
+      .once('receipt', (receipt) => {
+        this.setState({ loading: false })
+      })
+  }
+
+  tipPost(id, tipAmount) {
+    this.setState({ loading: true })
+    this.state.socialNetwork.methods.tipPost(id).send({ from: this.state.account, value: tipAmount })
+      .once('receipt', (receipt) => {
+        this.setState({ loading: false })
+      })
   }
 
   constructor(props) {
     super(props)
     this.state = {
-      account: ''
+      account: '',
+      socialNetwork: null,
+      postCount: 0,
+      posts: [],
+      loading: true
     }
+    this.createPost = this.createPost.bind(this)
+    this.tipPost = this.tipPost.bind(this)
   }
 
   render() {
     return (
       <div>
         <Navbar account={this.state.account} />
+        {this.state.loading
+          ? <div id="loader" className="text-center mt-5">Loading...</div>
+          : <Main
+            posts={this.state.posts}
+            createPost={this.createPost}
+            tipPost={this.tipPost}
+          />
+        }
       </div>
     );
   }
