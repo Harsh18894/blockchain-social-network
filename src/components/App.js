45ba371aca79a50c0a43557881c2b54a42767269
Harsh18894 <harshdeep18894@gmail.com>
2020-04-19 21:28:16 +0530

    Few changes

--- src/components/App.js
+++ src/components/App.js
@@ -9,11 +9,11 @@ class App extends Component {
         <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
           <a
             className="navbar-brand col-sm-3 col-md-2 mr-0"
-            href="http://www.dappuniversity.com/bootcamp"
+            href="https://github.com/Harsh18894/blockchain-social-network"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Dapp University
+            Social Network Project
           </a>
         </nav>
         <div className="container-fluid mt-5">
@@ -21,23 +21,23 @@ class App extends Component {
             <main role="main" className="col-lg-12 d-flex text-center">
               <div className="content mr-auto ml-auto">
                 <a
-                  href="http://www.dappuniversity.com/bootcamp"
+                  href="https://github.com/Harsh18894/blockchain-social-network"
                   target="_blank"
                   rel="noopener noreferrer"
                 >
                   <img src={logo} className="App-logo" alt="logo" />
                 </a>
-                <h1>Dapp University Starter Kit</h1>
+                <h1>Social Network Starter Kit</h1>
                 <p>
                   Edit <code>src/components/App.js</code> and save to reload.
                 </p>
                 <a
                   className="App-link"
-                  href="http://www.dappuniversity.com/bootcamp"
+                  href="https://github.com/Harsh18894/blockchain-social-network"
                   target="_blank"
                   rel="noopener noreferrer"
                 >
-                  LEARN BLOCKCHAIN <u><b>NOW! </b></u>
+                  <u><b>View Repo </b></u>
                 </a>
               </div>
             </main>
